d082a8cc3072a671138aa1986d683989262859e7
hank <henrik@hankolsen.com>
2017-09-26 22:20:40 +0200

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+	entry: "./js/app.js",
+	output: {
+		path: __dirname + "/public",
+		filename: "bundle.js"
+	},
+	module: {
+		loaders: [
+			{
+				test: /\.js$/,
+				loader: 'babel-loader',
+				exclude: /node_modules/
+			}
+		]
+	}	
+};

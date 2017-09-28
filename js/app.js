ab20aaec8ba90584f9fb2d9d77b486eb7102e758
hank <henrik@hankolsen.com>
2017-09-28 22:26:35 +0200

    Add Relay

--- js/app.js
+++ js/app.js
@@ -1,5 +1,16 @@
 import React from 'react';
 import { render } from 'react-dom';
+import Relay from 'react-relay';
 import Main from './components/Main';
 
 render(<Main limit={6}/>, document.getElementById('react'));
+
+console.log(
+	Relay.QL`
+		query Test {
+			links {
+				title
+			}
+		}		
+	`
+);
--- server.js
+++ server.js
@@ -1,7 +1,10 @@
 import express from 'express';
-import schema from './data/schema';
+import fs from 'fs';
+import Schema from './data/schema';
 import GraphQLHTTP from 'express-graphql';
 import { MongoClient } from 'mongodb';
+import {graphql} from 'graphql';
+import {introspectionQuery} from 'graphql/utilities';
 
 const app = express();
 
@@ -9,10 +12,20 @@ app.use(express.static('public'));
 
 (async () => {
   let db = await MongoClient.connect('mongodb://localhost/rgr');
+  let schema = Schema(db);
+
   app.use('/graphql', GraphQLHTTP({
-    schema: schema(db),
+    schema,
     graphiql: true
   }));
 
   app.listen(3000, () => console.log('Listining on port 3000'));
+
+  // Generate schema.json
+  const json = await graphql(schema, introspectionQuery);
+  fs.writeFile('./data/schema.json', JSON.stringify(json, null, 2), err => {
+    if (err) throw err;
+
+    console.log('JSON schema created');
+  })
 })();
